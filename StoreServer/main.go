04db898aa686df19cb147d8baba0f1a463d7ddf1
onohite <yeroxo@gmail.com>
2022-03-14 20:08:02 +0500

    16% of fish

--- StoreServer/main.go
+++ StoreServer/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/categories", handler) // each request calls handler
+
+	r := mux.NewRouter()
+	r.HandleFunc("/categories/{id}", handler2)
+	r.HandleFunc("/categories", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -24,6 +28,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		resp := new(Response)
 		resp.Categories = append(resp.Categories)
-		fmt.Fprintf(w, "[{\"Name\":\"privetpoka\",\"Products\":null,\"SubCategories\":null,\"_id\":\"622e676c066b4bf39fc2047b\",\"id\":40},{\"Name\":\"Ноутбук\",\"Products\":[{\"ImageUrl\":\"123.com\",\"Name\":\"12312\",\"Price\":125.6,\"id\":1},{\"ImageUrl\":\"https://mobile-review.com/articles/2020/image/acer-aspire-5/pic/2.jpg\",\"Name\":\"Acer\",\"Price\":123.5,\"id\":2}],\"SubCategories\":[{\"Name\":\"Русские\"},{\"Name\":\"Китайские\"}],\"_id\":\"622e692ee0f80bdd6793d527\",\"id\":1},{\"Name\":\"Холодильники\",\"Products\":[{\"ImageUrl\":\"https://static.onlinetrade.ru/img/fullreviews/53166/1_big.jpg\",\"Name\":\"New Eva\",\"Price\":12005.6,\"id\":3},{\"ImageUrl\":\"https://img.mvideo.ru/Pdb/20070442b.jpg\",\"Name\":\"Old Mod\",\"Price\":12312.5,\"id\":4}],\"SubCategories\":[{\"Name\":\"С морозилкой\"},{\"Name\":\"Умные\"}],\"_id\":\"622e6970e0f80bdd679453c5\",\"id\":2}]")
+		fmt.Fprintf(w, "[{\n  \"id\":1,\n  \"Name\":\"Samsung\",\n  \"ImageUrl\":\"123.com\",\n  \"Price\": 125.6,\n  \"Category\": 3\n},\n{\n  \"id\":2,\n  \"Name\":\"Huawei\",\n  \"ImageUrl\":\"1235.com\",\n  \"Price\": 12523.6,\n  \"Category\": 3\n}]\n")
+	}
+}
+
+func handler2(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		vars := mux.Vars(r)
+		_, ok := vars["id"]
+		if !ok {
+			fmt.Println("id is missing in parameters")
+		}
+		fmt.Fprintf(w, "[\n  {\n    \"id\": 1,\n    \"Name\": \"Ноутбуки\",\n    \"SubCategories\": [\n      {\n        \"id\": 2\n      }\n    ]\n  },\n  {\n    \"id\": 2,\n    \"Name\": \"Адаптеры питания\",\n    \"SubCategories\": null\n  },\n  {\n    \"id\": 3,\n    \"Name\": \"Телефоны\",\n    \"SubCategories\": null\n  }\n]")
 	}
 }
